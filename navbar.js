fd1d95629cfabfc91a3322e8d45a485b8df479be
aryan-ali166211 <arygghost@gmail.com>
2025-04-10 06:38:30 -0700

    update

---
+++ navbar.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const hamburger = document.querySelector(".hamburger");
+//   const navList = document.querySelector(".header-container nav ul");
+
+//   hamburger.addEventListener("click", () => {
+//     navList.classList.toggle("active");
+//   });
+// });
